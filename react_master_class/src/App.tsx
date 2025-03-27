341973d24aafea5932eec24db8b662f28861e027
jisu kim <rla2877622@naver.com>
2025-03-27 17:28:50 +0900

    Feat: trello study
    FEAT : useRef, react-hook-form, recoil...코드 챌린지는 새로운 프로젝트 파서 깃헙io로 배포 예정

--- react_master_class/src/App.tsx
+++ react_master_class/src/App.tsx
@@ -1,69 +1,9 @@
-import { DragDropContext, Droppable, DropResult } from "react-beautiful-dnd";
-import { useRecoilState } from "recoil";
-import styled from "styled-components";
-import { ToDoAtom } from "./trello/recoil";
-import Card from "./trello/components/Card";
-
-const Wrap = styled.main`
-  display: grid;
-  grid-template-columns: repeat(1, 3fr);
-  padding: 20px;
-  justify-content: center;
-  gap: 20px;
-`;
-
-const Board = styled.ul`
-  padding: 20px 10px;
-  background-color: ${({ theme }) => theme.bgGray};
-  border-radius: 5px;
-  display: flex;
-  flex-direction: column;
-  gap: 10px;
-  min-width: 280px;
-  max-width: 320px;
-  flex-shrink: 0;
-`;
+import TrelloApp from "./trello/TrelloApp";
 
 function App() {
 
-  const [toDos, setToDos] = useRecoilState<string[]>(ToDoAtom);
-
-  const dragFunc = ({ destination, source }: DropResult) => {
-
-    if (!destination) return;
-
-    setToDos(tmpToDos => {
-
-      const currentIdx = Number(source.index);
-      const targetIdx = Number(destination?.index);
-
-      const copied = [...tmpToDos]; 
-      const [moved] = copied.splice(currentIdx, 1);
-      copied.splice(targetIdx, 0, moved); 
-
-      return copied;
-    });
-  };
-
   return (
-    <DragDropContext onDragEnd={dragFunc}>
-      <Wrap>
-        <Droppable droppableId="oneDrop">
-          {(dropProps) => (
-            <Board ref={dropProps.innerRef} {...dropProps.droppableProps}>
-              {toDos.map((toDo, idx) => (
-                <Card
-                  key={toDo} 
-                  toDo={toDo}
-                  idx={idx}
-                />
-              ))}
-              {dropProps.placeholder}
-            </Board>
-          )}
-        </Droppable>
-      </Wrap>
-    </DragDropContext>
+    <TrelloApp />
   );
 }
 
--- react_master_class/src/trello/components/Card.tsx
+++ react_master_class/src/trello/components/Card.tsx
@@ -1,29 +1,33 @@
 import React from "react";
 import { Draggable } from "react-beautiful-dnd";
 import styled from "styled-components";
-
-interface ICard {
+export interface ICard {
     toDo: string;
+    id: number;
     idx: number;
 }
 
-const CardEl = styled.li`
+const CardEl = styled.li<{ is_dragging: string }>`
     border-radius: 5px;
     padding: 10px 10px;
-    background-color: ${({ theme }) => theme.cardBg};
-    box-shadow: 0 4px 10px ${({ theme }) => theme.cardShadow};
+    background-color: ${(props) =>
+        props.is_dragging === "true" ? "#111" : props.theme.bgDark};
+    box-shadow: ${(props) =>
+        props.is_dragging === "true" ? "0px 2px 5px rgba(0, 0, 0, 0.5)" : "none"};
     transition: box-shadow 0.2s ease;
+    font-weight: 700;
 `;
 
-const Card = ({ toDo, idx }: ICard) => {
+const Card = ({ toDo, id, idx }: ICard) => {
 
     return (
-        <Draggable draggableId={toDo} index={idx}>
-            {(dragProps) =>
+        <Draggable draggableId={`${id}`} index={idx}>
+            {(dragProps, snapshot) =>
                 <CardEl
                     ref={dragProps.innerRef}
                     {...dragProps.draggableProps}
                     {...dragProps.dragHandleProps}
+                    is_dragging={snapshot.isDragging.toString()}
                 >
                     {toDo}
                 </CardEl>
